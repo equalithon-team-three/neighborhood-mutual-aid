6c3373490fcb8c217d1191c73af77dc80c4f0b50
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-04-01 12:46:03 -0400

    makes CORS policy more nuanced

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins Rails.configuration.cors_origins
 
     resource '*',
       headers: :any,
