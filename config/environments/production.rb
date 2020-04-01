2bbf39eca1863ff7db437daf5919d6a3f277a08e
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-04-01 14:31:42 -0400

    no slash at the end of cors config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.cors_origins = [ "https://tr-aid.web.app/" ]
+  config.cors_origins = [ "https://tr-aid.web.app" ]
 
 end
