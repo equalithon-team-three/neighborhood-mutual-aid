0bfab826c20b84165218d58703b4f34f4cf248b1
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-02 00:21:44 -0400

    feat: created the RESTful /api/users/:userId/posts GET route to fetch a sorted hash consisting of an array of current user posts and an array of all other users posts to minimize the amount of request made to the server when fetching said data

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,8 +2,15 @@ class Api::PostsController < ApplicationController
   before_action :find_post, only: [:update, :destroy, :show, :edit]
 
   def index
-    @posts = Post.all 
-    render json: @posts
+    @posts = Post.all
+
+    if User.exists? params["user_id"]
+      user_posts = @posts.where user_id: params["user_id"]
+      other_posts = @posts.where.not user_id: params["user_id"]
+      @posts = {user_posts: user_posts, other_posts: other_posts}
+    end
+
+    render json: @posts, include: :user
   end
 
   def new
@@ -16,22 +23,18 @@ class Api::PostsController < ApplicationController
     render json: @post, status: :accepted
   end
 
-  def show 
-    render json: @post 
-  end 
+  def show
+    render json: @post
+  end
 
   def edit
-  end 
+  end
 
-  def update 
-  end 
+  def update
+  end
 
-  def destroy 
-  end 
-
-
-
-
+  def destroy
+  end
 
   # t.string :title
   # t.boolean :request_offer
@@ -44,17 +47,12 @@ class Api::PostsController < ApplicationController
 
   private
 
-  def post_params 
+  def post_params
     params.require(:user_id).permit(:title, :request_offer, :details, :date_posted, :location, :quantity, :deadline, :completed)
-  end 
+  end
 
-  def find_post 
+  def find_post
     @post = Post.find(params[:id])
-  end 
-  
-
-
-
-
+  end
 
 end
