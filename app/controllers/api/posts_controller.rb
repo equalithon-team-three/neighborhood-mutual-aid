b962b9a0816041242cd2de348eeec7ff53abb2ed
Tal Luigi <luigi.t.agr@gmail.com>
2020-04-04 22:28:02 -0400

    bugfix: removed || nil from @posts.where in not completed search query in posts controller post removal of nil as default value of that field in the database

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,12 +4,12 @@ class Api::PostsController < ApplicationController
   def index
     @posts = Post.incomplete
     user_id = params["user_id"].to_i
-    
+
     if user_id && User.exists?(user_id)
       return head :unauthorized unless logged_in_as?(user_id)
-      
+
       user_posts_completed = @posts.where({user_id: user_id, completed: true})
-      user_posts_not_completed = @posts.where({user_id: user_id, completed: false || nil})
+      user_posts_not_completed = @posts.where({user_id: user_id, completed: false})
       other_posts_not_completed = @posts.where.not({user_id: user_id}, {completed: true})
 
       @posts = {user_posts: {completed: user_posts_completed, not_completed: user_posts_not_completed}, other_posts: other_posts_not_completed}
