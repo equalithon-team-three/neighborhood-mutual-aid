73d460462281376835aabdd42a6a2a84679bece0
Tal Luigi <luigi.t.agr@gmail.com>
2021-05-14 00:04:59 -0400

    chore: added comments in application.rb and omniauth.rb

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,10 @@ module Traid
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.middleware.use ActionDispatch::Cookies
+    # config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
